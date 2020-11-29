b7cc266081d77176327e196c199ac860799340b3
Cheespeezah <71582070+Cheespeezah@users.noreply.github.com>
2020-11-29 13:16:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+People_array=[]
+var images=(
+"https://www.google.com/imgres?imgurl=http%3A%2F%2Fs3.amazonaws.com%2Fpix.iemoji.com%2Fimages%2Femoji%2Fapple%2Fios-12%2F256%2Fadult-light-skin-tone.png&imgrefurl=http%3A%2F%2Fwww.iemoji.com%2Fview%2Femoji%2F2528%2Fskin-tones%2Fadult-light-skin-tone&tbnid=MMwqDayJz6B46M&vet=12ahUKEwi90IKvn6ftAhVhNHIKHY4cAXgQMygEegUIARC-AQ..i&docid=WAMXOHaHwJz4IM&w=256&h=256&q=brother%20emoji&ved=2ahUKEwi90IKvn6ftAhVhNHIKHY4cAXgQMygEegUIARC-AQ" 
+,
+"https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.squarespace-cdn.com%2Fcontent%2Fv1%2F54dd407be4b0d35f6cfef75e%2F1452993421899-JTZRK5YVY3QHVNJ1UZ7G%2Fke17ZwdGBToddI8pDm48kNKSPxW9-3SXDfcM11sbE0dZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpyCfWi165zuIRRZjICpoVn7yH_7aCHB8bd94x91LewuGbpAkxc7zEKs_H7qxWdXsOQ%2FEmoji_Round_2_Suzette.png&imgrefurl=http%3A%2F%2Fmccauleycreativellc.com%2Fseinfeld&tbnid=PeZ4NXLRZ9FR0M&vet=12ahUKEwjEwo3_n6ftAhWpm0sFHcrcCXgQMyg8egQIARBa..i&docid=PF0tz6UiMzOGgM&w=667&h=667&q=mother%20emoji&ved=2ahUKEwjEwo3_n6ftAhWpm0sFHcrcCXgQMyg8egQIARBa"
+,
+"https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.iconbolt.com%2Fpreview%2Ftwitter%2Femoji%2Fbad-think-emoji-emoticon-expression-father-suspicious-think.svg&imgrefurl=https%3A%2F%2Fwww.iconbolt.com%2Ficonsets%2Femoji%2Fbad-think-emoji-emoticon-expression-father-suspicious-think&tbnid=z28YLWWAtAAHwM&vet=12ahUKEwjSgYWmoKftAhXhjEsFHWv9D7UQMyhcegUIARCkAQ..i&docid=BWsorWFIUwIejM&w=800&h=400&q=father%20emoji&ved=2ahUKEwjSgYWmoKftAhXhjEsFHWv9D7UQMyhcegUIARCkAQ"
+);
+
+function Change(){
+    var i=0;
+    document.getElementById("Howdy").src=images[i];
+    document.getElementById("hello").innerHTML=reasons[i];
+    i++;
+  }
